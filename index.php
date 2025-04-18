b8572fb7d49af0bae79eeca3330721f51bf63c07
HanjolineJ <Hanjolinej@gmail.com>
2025-04-18 09:35:41 -0400

    add index php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="css/my_styles.css" />
+</head>
+<body>
+    <header>
+        <h1>Welcome to Our Site</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="manage_services.php">Manage Services</a></li>
+            <li><a href="manage_treatments.php">Manage Treatments</a></li>
+            <li><a href="add_service.php">Add Service</a></li>
+            <li><a href="add_treatment.php">Add Treatment</a></li>
+        </ul>
+    </nav>
+    <section>
+        <p>This is the main page. Use the navigation menu to manage services or treatments.</p>
+    </section>
+    <footer>
+        &copy; 2025
+    </footer>
+</body>
+</html>
